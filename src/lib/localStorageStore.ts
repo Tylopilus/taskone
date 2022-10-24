a08613ab2b3dbb055b4114cc0034261244f1e42e
Helge <elitaer@gmail.com>
2022-10-24 18:58:58 +0200

    refactor

--- src/lib/localStorageStore.ts
+++ src/lib/localStorageStore.ts
@@ -4,13 +4,12 @@ import type { Tasks } from './store';
 
 export const persistLocalStorageStore = (key: string, initial: Tasks) => {
 	if (!browser) return writable(initial);
+
 	const persist = localStorage.getItem(key);
 	const data = persist ? JSON.parse(persist) : initial;
 	const store = writable<Tasks>(data, () => {
 		const unsub: Unsubscriber = store.subscribe((value: Tasks) => {
-			if (browser) {
-				localStorage.setItem(key, JSON.stringify(value));
-			}
+			localStorage.setItem(key, JSON.stringify(value));
 		});
 		return unsub;
 	});
