5f4489c39567c5415215cd834a2d7dfd11723166
Helge <elitaer@gmail.com>
2022-10-24 15:22:17 +0200

    add getTask and updateTask functions in store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,3 +1,4 @@
+import { get } from 'svelte/store';
 import { persistLocalStorageStore } from './localStorageStore';
 export type Task = {
 	id: string;
@@ -16,3 +17,21 @@ const tasks: Tasks = {
 	nextSession: []
 };
 export const store = persistLocalStorageStore('tasks', tasks);
+
+export const getTask = (id: string) => {
+	const task =
+		get(store).nextSession.find((task) => task.id === id) ||
+		get(store).currentSession.find((task) => task.id === id);
+	return task;
+};
+
+export const updateTask = (id: string, taskInput: Task) => {
+	const tasks = get(store);
+	const currentSession = tasks.currentSession.map((task) =>
+		task.id === id ? { ...task, ...taskInput } : task
+	);
+	const nextSession = tasks.nextSession.map((task) =>
+		task.id === id ? { ...task, ...taskInput } : task
+	);
+	store.set({ currentSession, nextSession });
+};
